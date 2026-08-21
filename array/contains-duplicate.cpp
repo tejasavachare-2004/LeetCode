f48d7becb51bbe0115d4a96d05f08120fd9397ca
Tejas Avachare <tejasavachare@gmail.com>
2026-08-21 14:08:37 +0530

    LC-contains-duplicate [Runtime: N/A; Memory: N/A]

---
+++ array/contains-duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       
+        unordered_set<int> seen ;
+
+
+        for(int n : nums){
+            if(seen.count(n)){
+                return true;
+            }
+            seen.insert(n);
+        }
+
+        return false;
+    };
+
+
+};
