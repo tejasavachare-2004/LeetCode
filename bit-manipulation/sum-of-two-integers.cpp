d67b7b19118d39a3704397c76fe416d200d52d07
Tejas Avachare <tejasavachare@gmail.com>
2026-09-16 15:49:58 +0530

    LC-sum-of-two-integers [Runtime: N/A; Memory: N/A]

--- bit-manipulation/sum-of-two-integers.cpp
+++ bit-manipulation/sum-of-two-integers.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
+        uint32_t carry = a & b;
+        int sumwithoutcarry = a^b;
+        int actual_carry = carry << 1;
+
+        while(carry != 0){
+            carry = sumwithoutcarry & actual_carry ;
+            sumwithoutcarry = sumwithoutcarry ^ actual_carry;
+            actual_carry = carry << 1;
+        }
+
+        return sumwithoutcarry;
     }
 };
